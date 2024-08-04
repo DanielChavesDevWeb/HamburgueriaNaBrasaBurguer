03f9417da25da3327805612df80ee262d45e77ff
Daniel Chaves <118618577+DanielChavesDevWeb@users.noreply.github.com>
2024-08-04 05:37:35 -0300

    initial-deploy

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'red-blood': '#B00000',
+        'green-sucess': '#44B000',
+        'grey-transparency': 'rgba(104, 104, 104, 0.184)',
+        'white-transparency': 'rgba(255, 255, 255, 0.205)'
+      }
+    },
+  },
+  plugins: [],
+}
