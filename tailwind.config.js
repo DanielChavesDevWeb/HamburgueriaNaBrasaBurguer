6f60ef5b6e09796a19e794a5a8c00d1885546eca
Daniel Chaves <118618577+DanielChavesDevWeb@users.noreply.github.com>
2024-08-05 05:15:55 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         'green-sucess': '#44B000',
         'grey-transparency': 'rgba(104, 104, 104, 0.184)',
         'white-transparency': 'rgba(255, 255, 255, 0.205)'
+      },
+      spacing:{
+        '432': '432px'
       }
     },
   },
